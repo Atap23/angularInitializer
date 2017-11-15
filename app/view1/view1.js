cc89eb97c3a62be2c11789eaf0ee97202efdf848
PC_ATAP <antonio.alvarez.torres@everis.com>
2017-11-15 02:15:22 +0100

    Haciendo esto funcionar.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -5,10 +5,30 @@ angular.module('angularInitializer.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'View1Ctrl',
+    controllerAs: 'view1'
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$http', '$window', function($http, $window) {
+  var vm = this;
+  vm.call = function () {
+
+    $http({
+      method : 'GET',
+      url : 'http://localhost:3000/surMano',
+      responseType: 'arraybuffer',
+      cache: false,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/zip'
+      }
+    }).then(function successCallback(response) {
+      console.log(response);
+      $window.open('http://localhost:3000/surMano');
+    }, function errorCallback(response) {
+      console.log(response);
+    });
+  };
 
 }]);
